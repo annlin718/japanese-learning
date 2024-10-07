254132c87af21c4f29840768889f80921e278bad
suisui2022 <suisuidesign2022@gmail.com>
2024-10-07 22:58:55 +0800

    部屬更改設定

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,15 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: "/japanese-learning/",
+    plugins: [
+        vue(),
+        vueJsx(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src',
+                import.meta.url))
+        }
     }
-  }
 })
